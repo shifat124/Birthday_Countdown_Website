a765f4a78d21bd1e332ff3b200e01db656bfd86f
Md. Shifat Laskor <69003347+shifat124@users.noreply.github.com>
2022-04-02 21:42:08 +0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const birthday = "24 September 2022";
+
+function countdown() {
+    const birthdayDate = new Date(birthday);
+    const currentDate = new Date();
+    const totalSeconds = (birthdayDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / (60 * 60 * 24));
+    const hours = Math.floor(totalSeconds % (3600 * 24) / (3600));
+    const minutes = Math.floor(totalSeconds % 3600 / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+
+
+    document.getElementById('days').innerText = formatTime(days);
+    document.getElementById('hours').innerText = formatTime(hours);
+    document.getElementById('minutes').innerText = formatTime(minutes);
+    document.getElementById('seconds').innerText = formatTime(seconds);
+
+
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
+}
+
+
+
+setInterval(countdown, 1000);
+countdown();
